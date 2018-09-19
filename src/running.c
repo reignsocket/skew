0751e947cbaf7e0f68c9afaec7530e62b5d8a2fa
reignsocket <lqwxml@163.com>
2018-09-19 11:16:11 +0800

    commit

---
+++ src/running.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	int i,k,base[31],sum;
+	char skew[32];
+	base[0]=1;
+	for( i = 1; i < 31; i++) base[i] = 2 * base[i-1] + 1;
+	while(1) {
+		scanf("%s", skew);
+		if (strcmp(skew,"0") == 0)
+			break;
+		sum = 0;
+		k = strlen(skew);
+		for( i = 0; i < strlen(skew); i++) {
+			k--;
+			sum += (skew[i] - '0') * base[k];
+		}
+		printf("%d\n",sum);
+	}
+	return 0;
+}
